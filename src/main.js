3a37407522ee7d66a3b76086c8fe5d2e09e1b667
sa871mil <sa871mil@htwg-konstanz.de>
2026-06-24 02:16:22 +0200

    Login verbesserungen2

--- src/main.js
+++ src/main.js
@@ -14,14 +14,16 @@ app.use(createPinia())
 app.use(router)
 
 
-
+const redirectUri = import.meta.env.DEV
+  ? 'http://localhost:5173'
+  : 'https://htwg-in-schneider.github.io/-frontend-moodify-/'
 
 app.use(
   createAuth0({
     domain: import.meta.env.VITE_AUTH0_DOMAIN,
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
     authorizationParams: {
-      redirect_uri: window.location.origin
+      redirect_uri: redirectUri
     }
   })
 )
